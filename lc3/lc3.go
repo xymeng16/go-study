ea5b9dc170e6faa0effe99f516e1c3a9ac5a07fb
Xiangyi Meng <xymeng16@gmail.com>
2022-01-24 17:22:24 +0800

    [lc3]: add some instructions

--- lc3/lc3.go
+++ lc3/lc3.go
@@ -1,12 +1,8 @@
 package main
 
-import "C"
 import (
 	"fmt"
-	"github.com/pkg/term/termios"
-	"golang.org/x/sys/unix"
 	"os"
-	"os/signal"
 )
 
 /* 65536 memory locations */
@@ -51,64 +47,13 @@ const (
 
 /* condition flags */
 const (
-	FL_POS uint8 = 1 << 0 /* positive */
-	FL_ZRO       = 1 << 1 /* zero */
-	FL_NEG       = 1 << 2 /* negative */
+	FL_POS uint16 = 1 << 0 /* positive */
+	FL_ZRO        = 1 << 1 /* zero */
+	FL_NEG        = 1 << 2 /* negative */
 )
 
 func read_image(f string) bool {
 	return false
-}
-
-func check_key() uint16 {
-	var readfds unix.FdSet
-
-	var timeout unix.Timeval
-	timeout.Sec = 0
-	timeout.Usec = 0
-
-	readfds.Zero()
-	readfds.Set(unix.Stdin)
-
-	n, err := unix.Select(1, &readfds, nil, nil, &timeout)
-	if err != nil {
-		return uint16(n)
-	} else {
-		return 0
-	}
-}
-
-var original_tio unix.Termios
-
-func disable_input_buffering() {
-	tmp_tio, _ := termios.Tcgetattr(uintptr(unix.Stdin))
-	original_tio = *tmp_tio
-	new_tio := original_tio
-	//fmt.Printf("%x\n", new_tio.Lflag & uint64(-265))
-	//new_tio.Lflag &= ^termios.ICANON & ^termios.ECHO // -101 & -9
-	new_tio.Lflag = 0x4c3
-
-	termios.Tcsetattr(uintptr(unix.Stdin), termios.TCSANOW, &new_tio)
-}
-
-func restore_input_buffering() {
-	termios.Tcsetattr(uintptr(unix.Stdin), termios.TCSANOW, &original_tio)
-}
-
-func handle_interrupt(signal int) {
-	restore_input_buffering()
-	fmt.Println()
-	os.Exit(-2)
-}
-
-func setup_sigint_handler() {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		<-c
-		fmt.Println("\r- Ctrl+C pressed in Terminal")
-		os.Exit(-2)
-	}()
 }
 
 func main() {
@@ -142,10 +87,9 @@ func main() {
 	running := true
 	for running {
 		/* FETCH */
-		instr := mem_read(reg[R_PC]++)
-		op := instr >> 12
+		instr := mem_read(reg[R_PC])
+		op = instr >> 12
 		reg[R_PC]++
-
 
 	}
 }
@@ -154,6 +98,6 @@ func mem_read(u uint16) uint16 {
 	return u
 }
 
-func exec_op(op uint16) error {
+func _(op uint16) {
 
 }
---
+++ lc3/utils.go
@@ -0,0 +1,18 @@
+package main
+
+func sign_extend(x uint16, bit_count int) uint16 {
+	if ((x >> (bit_count - 1)) & 1) == 1 {
+		x |= 0xFFFF << bit_count
+	}
+	return x
+}
+
+func update_flags(r uint16) {
+	if reg[r] == 0 {
+		reg[R_COND] = FL_ZRO
+	} else if ((reg[r] >> 15) & 1) == 1 {
+		reg[R_COND] = FL_NEG
+	} else {
+		reg[R_COND] = FL_POS
+	}
+}
